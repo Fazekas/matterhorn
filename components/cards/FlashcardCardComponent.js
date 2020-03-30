79b879061f611a840e0f55c2bc3b3d0422ea597d
nfazekas <fazekas.nick101@gmail.com>
2020-03-30 12:13:54 -0400

    Added animations to flashcard to flip when they are pressed

--- components/cards/FlashcardCardComponent.js
+++ components/cards/FlashcardCardComponent.js
@@ -1,25 +1,80 @@
 import React, { Component } from 'react';
-import { StyleSheet } from 'react-native';
+import { StyleSheet, Animated, TouchableOpacity } from 'react-native';
 import Card from './Card';
 import TextComponent from '../baseComponents/TextComponent';
 
 export default class FlashcardCardComponent extends Component {
   constructor(props) {
     super(props);
+
+    this.animatedValue = new Animated.Value(0);
+    this.value = 0;
+    this.animatedValue.addListener(({ value }) => {
+      this.value = value;
+    });
+    this.frontInterpolate = this.animatedValue.interpolate({
+      inputRange: [0, 180],
+      outputRange: ['0deg', '180deg'],
+    });
+
+    this.backInterpolate = this.animatedValue.interpolate({
+      inputRange: [0, 180],
+      outputRange: ['180deg', '360deg'],
+    });
   }
 
+  flipCard = () => {
+    if (this.value >= 90) {
+      Animated.spring(this.animatedValue, {
+        useNativeDriver: true,
+        toValue: 0,
+        friction: 8,
+        tension: 10,
+      }).start();
+    } else {
+      Animated.spring(this.animatedValue, {
+        useNativeDriver: true,
+        toValue: 180,
+        friction: 8,
+        tension: 10,
+      }).start();
+    }
+  };
+
   render() {
+    const frontAnimatedStyle = {
+      transform: [{ rotateY: this.frontInterpolate }],
+    };
+    const backAnimatedStyle = {
+      transform: [{ rotateY: this.backInterpolate }],
+    };
+
     return (
-      <Card cardStyle={styles.cardStyle}>
-        <TextComponent style={styles.alignText}>
-          {this.props.name}
-        </TextComponent>
-      </Card>
+      <TouchableOpacity
+        style={styles.fullWidth}
+        onPress={() => this.flipCard()}>
+        <Card
+          cardStyle={[styles.cardStyle, styles.flipCard, frontAnimatedStyle]}>
+          <TextComponent>{this.props.term}</TextComponent>
+        </Card>
+        <Card
+          cardStyle={[
+            backAnimatedStyle,
+            styles.cardStyle,
+            styles.flipCard,
+            styles.flipCardBack,
+          ]}>
+          <TextComponent>{this.props.definition}</TextComponent>
+        </Card>
+      </TouchableOpacity>
     );
   }
 }
 
 const styles = StyleSheet.create({
+  fullWidth: {
+    width: '100%',
+  },
   cardStyle: {
     marginHorizontal: 0,
     marginBottom: 25,
@@ -27,5 +82,12 @@ const styles = StyleSheet.create({
   },
   alignText: {
     textAlign: 'center',
+  },
+  flipCard: {
+    backfaceVisibility: 'hidden',
+  },
+  flipCardBack: {
+    position: 'absolute',
+    top: 0,
   },
 });
